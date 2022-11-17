df14b51a33b48022ce0c6ef4e5ac392d79c3da69
yaochong <you@example.com>
2022-11-17 09:15:27 +0800

    移出service worker注册文件registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App isExt={false}/>, document.getElementById('root'));
-registerServiceWorker();
